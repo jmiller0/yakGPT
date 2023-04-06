14d372fd5d4f8059db0596de7495b37094757336
jmiller0 <github@miller.center>
2023-04-06 11:46:34 -0400

    add basic auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import ChatDisplay from "@/components/ChatDisplay";
 import Hero from "@/components/Hero";
 import { useChatStore } from "@/stores/ChatStore";
+import withBasicAuth from "@/pages/withBasicAuth";
 
-export default function Home() {
+export function Home() {
   const apiKey = useChatStore((state) => state.apiKey);
 
   return apiKey ? <ChatDisplay /> : <Hero />;
 }
+
+export default withBasicAuth(Home);
